edf0dac481253e44af4376010230f0cbf33b22d9
prithwirajpm <wt.prithwiraj@gmail.com>
2023-10-17 13:12:00 +0530

    Add Cart

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { Row, Col, Card, Button } from 'react-bootstrap'
 
 function Home() {
   return (
-    <div>Home</div>
+    <>
+      <Row className='mt-5 mb-5'>
+        <Col>
+          <Card style={{ width: '18rem' }} className='ms-5'>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+             <div className='d-flex justify-content-between'>
+                <Button className='btn btn-light'><i class="fa-solid fa-heart text-danger"></i></Button>
+                <Button className='btn btn-light'><i class="fa-solid fa-cart-plus text-success"></i></Button>
+             </div>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </>
   )
 }
 
